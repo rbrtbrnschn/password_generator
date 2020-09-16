ed9d483ab2f632b5ec72ac21f512429218c33e16
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-09-16 11:48:30 +0200

    updated uninstall.sh to remove local dir

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 sudo rm -rf /usr/local/bin/password_generator/
 sudo rm -rf /usr/local/bin/pass-gen
+sudo rm -rf ~/database.csv
+sudo rm -rf ~/.database.csv
+
 echo "uninstall complete"
+echo "Remove Current Directory? [y/n]"
+read BOO
+
+case $BOO in
+	[y] | [yY][eE][sS])
+		PWD=$(pwd)
+		sudo rm -rf "$PWD"
+		cd ..
+		;;
+	[n] | [nN][oO])
+		:
+		;;
+	*)
+		echo "Please answer with y/yes or n/no"
+		./uninstall.sh
+		;;
+esac
+clear
+
